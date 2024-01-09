3f3097c87760bb75d44cc660bb7ab2e3927e5532
Sasha Koss <koss@nocorp.me>
2024-01-09 13:26:36 +0800

    Import blog structure from date-fns blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,17 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
   i18n: defineCollection({ type: "data", schema: i18nSchema() }),
+  blog: defineCollection({
+    type: "content",
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: z.string().optional(),
+    }),
+  }),
 };
